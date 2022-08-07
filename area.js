cf01483487fbf727904c7f6f1e80122febe89baa
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-08-07 16:23:43 +0600

    desimalnumber

--- area.js
+++ area.js
@@ -1,1 +1,7 @@
-function areaOfcercle (r)
+function areaOfcercle(r) {
+    const pi = 3.1416;
+    const area = pi * r * r;
+    return area.toFixed(2);
+}
+console.log(areaOfcercle(7));
+console.log(areaOfcercle(9));
